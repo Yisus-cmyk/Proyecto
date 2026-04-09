562f484186c6dc3b15f8c0ea2f70f8cecbe6c980
unknown <j.honrubiajulia@gmail.com>
2026-04-09 14:15:21 +0200

    version con parallaxText en el nav, sin js en el logo

---
+++ js/funciones.js
@@ -0,0 +1,31 @@
+//Movimiento del ratón
+const cursor = document.querySelector(".cursor-personalizado");
+
+document.addEventListener("mousemove", (e) => {
+    cursor.style.setProperty("--x", e.clientX + "px" )
+    cursor.style.setProperty("--y", e.clientY + "px" )
+});
+
+const deslizarDrch = document.querySelector(".trazo p");
+        const deslizarIzq = document.querySelector(".bruto p");
+        let scrollActual = 0;
+        let targetScroll = 0;
+        const ease = 0.8;
+
+        function AnimacionTipo(){
+            targetScroll = window.scrollY;
+            scrollActual += (targetScroll - scrollActual) * ease;
+
+            const widthDrch = deslizarDrch.offsetWidth / 2;
+            const widthIzq = deslizarIzq.offsetWidth / 2;
+
+            const xDrch = -widthDrch + (scrollActual * 0.2 % widthDrch);
+            const xIzq = -(scrollActual * 0.2 % widthIzq);
+
+            deslizarDrch.style.transform = `translateX(${xDrch}px)`;
+            deslizarIzq.style.transform = `translateX(${xIzq}px)`;
+        requestAnimationFrame(AnimacionTipo);
+        };
+        AnimacionTipo();
+
+console.log("Contenido cargado correctamente.")
