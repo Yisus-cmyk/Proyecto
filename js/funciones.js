722154f00770dbafc1abe9e2ab1eef6871e86333
jesus_honrubia <j.honrubiajulia@gmail.com>
2026-04-10 21:32:17 +0200

    version con el header main acabado, cursor y animaciones iniciales terminadas

--- js/funciones.js
+++ js/funciones.js
@@ -5,6 +5,13 @@ document.addEventListener("mousemove", (e) => {
     cursor.style.setProperty("--x", e.clientX + "px" )
     cursor.style.setProperty("--y", e.clientY + "px" )
 });
+document.addEventListener("mouseover", (e) => {
+    if(e.target.closest("a, button")){
+        cursor.classList.add("cursor-activo")
+    }else{
+        cursor.classList.remove("cursor-activo")
+    }
+});
     //Animacion Parallax Typo
 const deslizarDrch = document.querySelector(".trazo p");
         const deslizarIzq = document.querySelector(".bruto p");
@@ -27,5 +34,43 @@ const deslizarDrch = document.querySelector(".trazo p");
         requestAnimationFrame(AnimacionTipo);
         };
         AnimacionTipo();
+
+    //Animación header
+    const header = document.querySelector("header");
+    let scrollDown = window.scrollY;
+
+    window.addEventListener("scroll", () => {
+        const scrollActualY = window.scrollY;
+        
+        if(scrollActualY > scrollDown && scrollActualY > 80){
+            header.classList.add("header-hidden")
+        }else{
+            header.classList.remove("header-hidden")
+        }
+        scrollDown = scrollActualY
+    });
+
+const logo = document.querySelector(".logo");
+
+document.addEventListener("mousemove", (e) => {
+    const posicion = logo.getBoundingClientRect();
+    const xCentro = posicion.left + (posicion.width / 2);
+    const yCentro = posicion.top + (posicion.height / 2);
+
+    const distanciaX = e.clientX - xCentro;
+    const distanciaY = e.clientY - yCentro;
+        //Rango de atracción
+    const rangoX = 300;
+    const rangoY = 80;
+
+    if(Math.abs(distanciaX) < rangoX && Math.abs (distanciaY) < rangoY){
+        const moverX = distanciaX * 0.10;
+        const moverY = distanciaY * 0.3;
+        logo.style.transform = `translate(${moverX}px, ${moverY}px)`;
+    }else{
+        logo.style.transform = `translate(0px, 0px)`;
+    }
+});
+
 
 console.log("Contenido cargado correctamente.")
