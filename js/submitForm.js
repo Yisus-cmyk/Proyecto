5a9b90c5903bb5ae5243ac93ed507aea6fb7103f
jesus_honrubia <j.honrubiajulia@gmail.com>
2026-04-14 19:53:35 +0200

    Creacion de logica para enviar datos de formulario

---
+++ js/submitForm.js
@@ -0,0 +1,25 @@
+function submitForm() {
+    const nombre = document.querySelector('input[name="nombre"]').value;
+    const correo = document.querySelector('input[name="correo"]').value;
+    const mensaje = document.querySelector('input[name="mensaje"]').value;
+
+    console.log(nombre,"nombre")
+    console.log(correo,"correo")
+    console.log(mensaje,"idea")
+
+    fetch("https://hook.eu1.make.com/elp6ioymxouae46u3rjq4rw6ijrkoowi", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "x-make-apikey":""
+        },
+        body: JSON.stringify({
+            nombre,
+            correo,
+            mensaje
+        })
+    })
+    .catch(err => {
+        console.error(err);
+    });
+}
